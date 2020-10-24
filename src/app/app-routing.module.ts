987745093ba936326939aa6749f13cbe6318a90e
Espe <ego.egarco01@gmail.com>
2020-10-24 12:51:14 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { Page404Component } from './page404/page404.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {path: 'dasboard', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: 'graphic1', component: Graphics1Component},
+      {path: '', redirectTo: '/dasboard', pathMatch: 'full'},
+    ]
+  },
+
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
